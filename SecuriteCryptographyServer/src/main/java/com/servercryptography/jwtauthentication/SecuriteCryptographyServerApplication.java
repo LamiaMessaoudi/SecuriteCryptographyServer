c7185a72ea465b0e78b06b5239019818a5f9db5d
pc <pc@DESKTOP-K49A36K.mshome.net>
2019-04-19 21:45:33 +0200

    Init project

---
+++ SecuriteCryptographyServer/src/main/java/com/servercryptography/jwtauthentication/SecuriteCryptographyServerApplication.java
@@ -0,0 +1,12 @@
+package com.servercryptography.jwtauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuriteCryptographyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuriteCryptographyServerApplication.class, args);
+	}
+}
